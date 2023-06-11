b28ceaf78e91afaaae939be048891ff1e21af748
Tibor Balogh <btibor@gmx.at>
2023-06-11 21:52:07 +0200

    save
    working on site-logic
    use main branch for development

--- src/DOM_Module.js
+++ src/DOM_Module.js
@@ -1,13 +1,12 @@
-import {createTodo, createProject} from "./TotoLogic_Module"
-
-const content = document.querySelector("content");
+import { createTodo, createProject } from "./TodoLogic_Module";
 
 //Creates the basic structure of the page
 function createHomeDOM() {
+    const content = document.querySelector("#content");
     const header = document.createElement("div");
         header.classList.add("header");
     content.appendChild(header);
-        const logo = document.querySelector("p");
+        const logo = document.createElement("p");
             logo.textContent = "Todo List"
         header.appendChild(logo);
 
@@ -23,13 +22,15 @@ function createHomeDOM() {
 function createSidebar(){
     const sidebar = document.querySelector(".sidebar");
     
-    const sidebarTop = document.querySelector("div");
+    const sidebarTop = document.createElement("div");
         sidebarTop.classList.add("sidebar-top");
     sidebar.appendChild(sidebarTop);
         const homeButton = document.createElement("button");
+            homeButton.id = "Home";
             homeButton.textContent = "Home";
         sidebarTop.appendChild(homeButton);
         const newProjectButton = document.createElement("button");
+            newProjectButton.id = "newProject"
             newProjectButton.textContent = "+ New Project";
         sidebarTop.appendChild(newProjectButton);
 
@@ -45,19 +46,65 @@ function createSidebar(){
         sidebarBottom.appendChild(impressumButton);
 }
 
-//Logic for sidebar
-function eventListenerSidebar(){
-    
-}
-
 //Logic for Main Display 
-function displayMain(project) {
+function displayTodo(todo) {
 
 }
+
+function displayMain(project) {
+    const main = document.querySelector(".main-page");
+    main.innerHTML = "";
+    const h1 = document.createElement("h1");
+        h1.textContent = project.name;
+    main.appendChild(h1);
+
+    const todoAddButton = document.createElement("button");
+        todoAddButton.textContent = "+ Todo";
+    main.appendChild(todoAddButton);
+}
+
+//Project Form
+function projectForm() {
+    const main = document.querySelector(".main-page");
+    main.innerHTML = "";
+    const projectForm = document.createElement("form");
+    projectForm.method = "#";
+    projectForm.id = "project-form";
+    const name = document.createElement("input");
+        name.id = "name";
+        name.value = "";
+        const nameLabel = document.createElement("label");
+            nameLabel.setAttribute("for", "name");
+            nameLabel.textContent = "Project Name";
+    projectForm.appendChild(name);
+    projectForm.appendChild(nameLabel);
+
+    const dueDate = document.createElement("input");
+        dueDate.id = "due-date";
+        dueDate.value = "";
+        const dueDateLabel = document.createElement("label");
+            dueDateLabel.setAttribute("for", "dueDate");
+            dueDateLabel.textContent = "due Date"
+    projectForm.appendChild(dueDate);
+    projectForm.appendChild(dueDateLabel);
+
+
+    const submit = document.createElement("button");
+        submit.id = "project-submit";
+        submit.type = "submit";
+        submit.textContent = "Create Project";
+    projectForm.appendChild(submit);
+
+    main.appendChild(projectForm);
+}
+
+//Todo Form
+
+
 
 export {
     createHomeDOM,
     createSidebar,
-    eventListenerSidebar,
-    displayMain
+    displayMain,
+    projectForm
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,54 @@
 import "./styles.css";
 import "./normalize.css";
+import {
+    createHomeDOM,
+    createSidebar,
+    displayMain,
+    projectForm
+} from "./DOM_Module";
+import {
+    createTodo,
+    createProject
+} from "./TodoLogic_Module"
+
+//handles all projects
+let projectArr = {}; 
+function addToProjectArr(object){
+    projectArr = {projectArr, object}
+};
+
+//Creates the Home page
+const home = createProject("Task for Today", null);
+addToProjectArr(home);
+createHomeDOM();
+createSidebar();
+displayMain(home);
+
+//Logic for sidebar buttons
+const homeBtn = document.querySelector("#Home")
+homeBtn.addEventListener("click", () => {
+    displayMain(home);
+});
+
+const newProject = document.querySelector("#newProject");
+newProject.addEventListener("click", (e) => {
+    projectForm();
+    handleProjectSubmit();
+})
+
+//Handle Porject Submit
+function handleProjectSubmit() {
+    const inputName = document.querySelector("#name");
+    const inputDueDate = document.querySelector("#due-date");
+    const projectForm = document.querySelector("#project-form");
+
+    projectForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const test = createProject(inputName.value, inputDueDate.value);
+        addToProjectArr(test);
+        console.log(projectArr);
+        displayMain(test);
+    }) 
+}
+
+
